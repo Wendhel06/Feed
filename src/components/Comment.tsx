b442cc4a0c3014080c34635807c94c3fc6b2884b
Wendhel Alves Nogueira <wendhelalves7@gmail.com>
2023-05-01 20:21:25 -0300

    Deploy

---
+++ src/components/Comment.tsx
@@ -0,0 +1,56 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+import { Avatar } from './Avatar';
+import { useState } from 'react';
+
+interface CommentsProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentsProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeCount() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        className={styles.avatar}
+        hasBorder={false}
+        src="https://github.com/Wendhel06.png"
+        alt=''
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Wendhel</strong>
+              <time title="17 de abril de 2023" dateTime="2023-04-17 09:13">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button onClick={handleDeleteComment} title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+        <footer>
+          <button onClick={handleLikeCount}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,26 @@
+import { Avatar } from './Avatar';
+import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=400&q=50"
+        alt=""
+      />
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/Wendhel06.png" />
+        <strong>Wendhel Alves</strong>
+        <span>Web Developer</span>
+        <footer>
+          <a href="#">
+            <PencilLine size={20} />
+            Editar seu perfil
+          </a>
+        </footer>
+      </div>
+    </aside>
+  );
+}
